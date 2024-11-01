28f9f81ab811794c0b7c0daf8aff3124dfc48cce
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-11-01 20:00:54 +0600

    added codechef contest Starters 158

--- Codechef/Starters 158/Even_vs_Odd_Divisors.cpp
+++ Codechef/Starters 158/Even_vs_Odd_Divisors.cpp
@@ -28,3 +28,28 @@ int main()
     
     return 0;
 }
+
+
+
+// Another Approach;
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t; cin >> t;
+    while (t--)
+    {
+        int n; cin >> n;
+        if(n%2==1) cout << -1 << endl;
+        else{
+            if(n%4) cout << 0 << endl;
+            else cout << 1 << endl;
+        }
+    }
+    
+    return 0;
+}
+
