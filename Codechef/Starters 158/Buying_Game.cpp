14b30585ac66117f4490755a70626f6cab433345
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-11-06 08:55:56 +0600

    updated codechef contest Starters 158

--- Codechef/Starters 158/Buying_Game.cpp
+++ Codechef/Starters 158/Buying_Game.cpp
@@ -50,3 +50,60 @@ int main()
     }
     return 0;
 }
+
+
+
+
+// Another approach from YT;
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vl;
+typedef vector<pair<ll, ll>> vpl;
+
+void solve(int testcase) {
+    ll n;
+    cin >> n;
+    vl x(n), y(n);
+
+    // Input each element in vector x and y separately
+    for (ll i = 0; i < n; i++) {
+        cin >> x[i];
+    }
+    for (ll i = 0; i < n; i++) {
+        cin >> y[i];
+    }
+
+    vpl vec(n);
+    ll sum = accumulate(y.begin(), y.end(), 0ll);
+    ll ans = sum;
+
+    for (ll i = 0; i < n; i++) {
+        vec[i] = {x[i] - y[i], y[i]};
+    }
+
+    sort(vec.begin(), vec.end());
+
+    sum += vec[0].first;
+    for (ll i = 1; i < n; i++) {
+        sum += vec[i].first;
+        ans = min(sum, ans);
+    }
+
+    cout << ans << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve(t);
+    }
+
+    return 0;
+}
