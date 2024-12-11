ca48470e314dc9fb919fa5f4c525e8e1939529c7
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-12-11 23:55:20 +0600

    added new contest Codeforces Round 991 (Div. 3)

---
+++ Codechef/STARTERS 164/Halloween_Array.cpp
@@ -0,0 +1,109 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--)
+    {
+        int n,l,r; cin >> n >> l >> r;
+        vector<int> v(n);
+        for(int i=0; i<n; i++){
+            cin >> v[i];
+        }
+
+        vector<int> pre(n+1, 0);
+        for(int i=1; i<=n; i++){
+            pre[i]=pre[i-1]^v[i-1];
+        }
+        ll mul=1;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                int XOR=pre[j]^pre[i];
+            }
+            if(mul>r){
+                break;
+            }
+        }
+        cout << (mul >= l && mul <= r ? "YES\n" : "NO\n");
+    }
+    
+    return 0;
+
+
+// Alhamdulillah
+}
+
+
+
+
+
+
+
+
+
+
+
+// #include<bits/stdc++.h>
+// #define ll long long
+// #define pi pair<ll, ll>
+// #define asort(v) sort(v.begin(), v.end())
+// #define rsort(v) sort(v.begin(), v.end(), greater<>())
+// #define yes cout << "YES\n"
+// #define no cout << "NO\n"
+// #define cyes cout << "Yes\n"
+// #define cno cout << "No\n"
+// #define endl "\n"
+// #define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+// using namespace std;
+// bool solve(vector<int> &v, int l, int r){
+//     ll mul=1; int n=v.size();
+//     for(int i=0; i<n; i++){
+//         for(int j=i+1; j<n; j++){
+//             int XOR=v[i]^v[j]; 
+//             mul*=XOR;           
+//         }
+//     }
+//     return (mul>=l && mul<=r);
+
+// }
+// int main()
+// {
+//     fast();
+//     ll t; cin >> t;
+//     while (t--)
+//     {
+//         int n, l, r; cin >> n >> l >> r;
+//         vector<int> v(n);
+//         for(ll i=0; i<n; i++){
+//             cin >> v[i];
+//         }
+//         cout << (solve(v, l, r) ? "YES" : "NO") << endl;
+//     }
+    
+//     return 0;
+// // Alhamdulillah
+// }
+
+
+
+
+
+
+
+
+
+
+
