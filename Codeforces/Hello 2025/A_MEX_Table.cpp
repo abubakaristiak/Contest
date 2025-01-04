eb23a6121b6f1c165973a33bae1a0990fb265781
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-01-04 21:44:46 +0600

    added new contest Hello 2025

---
+++ Codeforces/Hello 2025/A_MEX_Table.cpp
@@ -0,0 +1,43 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-01-04 || 21:00:14
+ * * * * File    : A_MEX_Table.cpp
+ */
+
+
+
+// https://codeforces.com/contest/2057/problem/A
+
+
+
+#include<bits/stdc++.h>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--)
+    {
+        int a,b; cin >> a >> b;
+        ll res=max(a,b)+1; 
+        cout << res << endl;
+    }
+    
+    return 0;
+
+
+// Alhamdulillah
+}
