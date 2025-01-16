015d2584ded0d43f84efb5517d4f8ba3d36120e4
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-01-16 23:36:35 +0600

    updated new contest on codechef starters 169

--- Codechef/STARTERS 169/Hamming_equivalent.cpp
+++ Codechef/STARTERS 169/Hamming_equivalent.cpp
@@ -1,10 +1,10 @@
-/*
- * Bismillahir Rahmanir Raheem
- *
- * * * * Coder   : abubakaristiak
- * * * * Created : 2025-01-15 || 21:43:07
- * * * * File    : Hamming_equivalent.cpp
- */
+// /*
+//  * Bismillahir Rahmanir Raheem
+//  *
+//  * * * * Coder   : abubakaristiak
+//  * * * * Created : 2025-01-15 || 21:43:07
+//  * * * * File    : Hamming_equivalent.cpp
+//  */
 
 
 
@@ -22,43 +22,6 @@
 #define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 
 using namespace std;
-int cntBits(int num){
-    int cnt=0;
-    while (num)
-    {
-        cnt+=(num & 1);
-        num=num >> 1;
-    }
-    return cnt;
-}
-
-
-
-bool isSorted(vector<int> &v){
-    map<int, vector<int>> bg;
-    for(int i=0; i<v.size(); i++){
-        int sb=cntBits(v[i]);
-        bg[sb].push_back(i);
-    }
-    for(auto &[sb, idx]:bg){
-        vector<int> val;
-        for(int i:idx){
-            val.push_back(v[i]);
-        }
-        sort(val.begin(), val.end());
-        for(int i=0; i<idx.size(); i++){
-            v[idx[i]] = val[i];
-        }
-    }
-    for(int i=1; i<v.size(); i++){
-        if(v[i]<v[i-1]){
-            return false;
-        }
-    }
-    return true;
-}
-
-
 int main()
 {
     fast();
@@ -67,10 +30,24 @@ int main()
     {
         int n; cin >> n;
         vector<int> v(n);
-        for(int i=0 ;i<n; i++){
+        for(int i=0; i<n; i++){
             cin >> v[i];
         }
-        cout << (isSorted(v) ? "Yes":"No") << endl; 
+
+        bool ok=true;
+        for(int i=0; i<n; i++){
+            if(v[i]!=i+1){
+                int x=__builtin_popcount(v[i]);
+                int y=__builtin_popcount(i+1);
+                if(x!=y){
+                    ok=false;
+                    break;
+                }
+            }
+        }
+        if(ok){
+            cyes;
+        }else cno;
     }
     
     return 0;
@@ -78,3 +55,82 @@ int main()
 
 // Alhamdulillah
 }
+
+
+
+
+
+
+
+
+// #include<bits/stdc++.h>
+// #define ll long long
+// #define pi pair<ll, ll>
+// #define asort(v) sort(v.begin(), v.end())
+// #define rsort(v) sort(v.begin(), v.end(), greater<>())
+// #define yes cout << "YES\n"
+// #define no cout << "NO\n"
+// #define cyes cout << "Yes\n"
+// #define cno cout << "No\n"
+// #define endl "\n"
+// #define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+// using namespace std;
+// int cntBits(int num){
+//     int cnt=0;
+//     while (num)
+//     {
+//         cnt+=(num & 1);
+//         num=num >> 1;
+//     }
+//     return cnt;
+// }
+
+
+
+// bool isSorted(vector<int> &v){
+//     map<int, vector<int>> bg;
+//     for(int i=0; i<v.size(); i++){
+//         int sb=cntBits(v[i]);
+//         bg[sb].push_back(i);
+//     }
+//     for(auto &[sb, idx]:bg){
+//         vector<int> val;
+//         for(int i:idx){
+//             val.push_back(v[i]);
+//         }
+//         sort(val.begin(), val.end());
+//         for(int i=0; i<idx.size(); i++){
+//             v[idx[i]] = val[i];
+//         }
+//     }
+//     for(int i=1; i<v.size(); i++){
+//         if(v[i]<v[i-1]){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+
+// int main()
+// {
+//     fast();
+//     int t; cin >> t;
+//     while (t--)
+//     {
+//         int n; cin >> n;
+//         vector<int> v(n);
+//         for(int i=0 ;i<n; i++){
+//             cin >> v[i];
+//         }
+//         cout << (isSorted(v) ? "Yes":"No") << endl; 
+//     }
+    
+//     return 0;
+
+
+// // Alhamdulillah
+// }
+
+
--- Codechef/STARTERS 169/Make_K_Most_Frequent.cpp
+++ Codechef/STARTERS 169/Make_K_Most_Frequent.cpp
@@ -11,19 +11,51 @@
 #define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 
 using namespace std;
+void solve(){
+    int n,k; cin >> n >> k;
+    vector<int> v(n);
+    vector<int> cnt(21);
+
+    for(int i=0; i<n; i++){
+        cin >> v[i];
+        cnt[v[i]]++;
+    }
+    int mx=*max_element(cnt.begin(), cnt.end());
+    if(cnt[k]==mx){
+        cout << 0 << endl;
+        return;
+    }
+    
+    vector<int> prefixCnt=cnt;
+    for(int i=0; i<n; i++){
+        mx=*max_element(prefixCnt.begin(), prefixCnt.end());
+        if(prefixCnt[k]==mx){
+            cout << 1 << endl;
+            return;
+        }
+        prefixCnt[v[i]]--;
+    }
+
+    vector<int> suffixCnt=cnt;
+    for(int i=n-1; i>=0; i--){
+        mx=*max_element(suffixCnt.begin(), suffixCnt.end());
+        if(suffixCnt[k]==mx){
+            cout << 1 << endl;
+            return;
+        }
+        suffixCnt[v[i]]--;
+    }
+    cout << 2 << endl;
+}
+
+
 int main()
 {
     fast();
     int t; cin >> t;
     while (t--)
     {
-        int n,k; cin >> n >> k;
-        vector<int> v(n);
-        for(int i=0; i<n; i++){
-            cin >> v[i];
-        }
-
-        
+        solve();
     }
     
     return 0;
