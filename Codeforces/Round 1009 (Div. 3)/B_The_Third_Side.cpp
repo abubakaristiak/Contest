749c380da56cf3b65ce9a2a8d35892b42dabca48
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-03-11 21:49:31 +0600

    added new contest on Codeforces round 1009

---
+++ Codeforces/Round 1009 (Div. 3)/B_The_Third_Side.cpp
@@ -0,0 +1,54 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-03-11 || 21:07:03
+ * * * * File    : B_The_Third_Side.cpp
+*/
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+    ll n; cin >> n;
+    vector<ll> v(n);
+    ll sum=0;
+    for(ll i=0; i<n; i++){
+        cin >> v[i];
+        sum+=v[i];
+    }
+    cout << sum-(n-1) << endl;
+}
+
+
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+
+
+// Alhamdulillah---(Ramadan Kareem)
+}
