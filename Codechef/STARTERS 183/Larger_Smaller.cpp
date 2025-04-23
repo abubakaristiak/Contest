1388edbb80822533cd6dd3b2294c7345a8340de7
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-04-23 21:54:59 +0600

    added new contest on codechef starters 183

---
+++ Codechef/STARTERS 183/Larger_Smaller.cpp
@@ -0,0 +1,62 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-04-23 || 20:40:58
+ * * * * File    : Larger_Smaller.cpp
+*/
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+    ll n; cin >> n;
+    vector<ll> v(n);
+    for(ll i=0; i<n; i++){
+        cin >> v[i];
+    }
+    set<int> st(v.begin(), v.end());
+    if(st.size()<2){
+        cout << 0 << endl;
+        return;
+    }
+    int mx=*max_element(st.begin(), st.end());
+    int mn=*min_element(st.begin(), st.end());
+    int cnt=mx-mn-1;
+    if(cnt>=0){
+        cout << cnt << endl;
+    }else cout << 0 << endl;
+}
+
+
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+
+
+// Alhamdulillah
+}
