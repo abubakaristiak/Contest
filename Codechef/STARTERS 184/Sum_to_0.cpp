7fa0f38de6bbe54270042682353bf6ab2d183837
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-04-30 21:55:09 +0600

    added new contest on codechef starters 184

---
+++ Codechef/STARTERS 184/Sum_to_0.cpp
@@ -0,0 +1,61 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-04-30 || 20:51:44
+ * * * * File    : Sum_to_0.cpp
+*/
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+    int n; cin >> n;
+    if(n==1){
+        cout << -1 << endl;
+    }else if(n%2==0){
+        for(int i=0; i<n/2; i++){
+            cout << "1 -1 ";
+        }
+        cout << endl;
+    }else{
+        cout << "1 2 -3 ";
+        for(int i=0; i<(n-3)/2; i++){
+            cout << "1 -1 ";
+        }
+        cout << endl;
+    }
+}
+
+
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+
+
+// Alhamdulillah
+}
