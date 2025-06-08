76d1a14d5d8449cdddfa97e9ff084b2f11e03573
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-06-08 23:08:49 +0600

    added new contest on codeforces round 1029

---
+++ Codeforces/Round 1029 (Div. 3)/D_Retaliation.cpp
@@ -0,0 +1,90 @@
+/*
+||-----------------------------------||
+||        Abu Bakar Istiak           ||
+||     Chattogram Polytechnic        ||
+||        Department of CST          ||
+||    abubakar119147@gmail.com       ||
+||-----------------------------------||
+*/
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define ull unsigned long long
+#define pi pair<ll, ll>
+#define vi vector<ll>
+#define vpi vector<pi>
+#define pb push_back
+#define endl "\n"
+#define sp " "
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define sz(x) (ll)(x).size()
+#define minus cout << -1 << endl
+#define zero cout << 0 << endl
+#define MAX 100000
+#define MOD 998244353
+#define afor(i,a,b) for (ll i = (a); i < (b); ++i)
+#define rfor(i,a,b) for (ll i = (a); i >= (b); --i)
+#define asort(v) sort((v).begin(), (v).end())
+#define rsort(v) sort((v).begin(), (v).end(), greater<>())
+#define fast() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve() {
+    int n; cin >> n;
+    vector<int> v(n);
+    for(int i=0; i<n; i++){
+        cin >> v[i];
+    }
+
+    int a=v[1]-v[0];
+    bool flag=true;
+    for(int i=2; i<n; i++){
+        if(v[i]-v[i-1]!=a){
+            flag=false;
+            break;
+        }
+    }
+    if(!flag){
+        no;
+        return;
+    }
+
+    int b=v[0]-a;
+    if(b<0 || b%(n+1)){
+        no;
+        return;
+    }
+
+    int c=b/(n+1);
+    int d=c+a;
+    if(c<0 || d<0){
+        no;
+        return;
+    }
+    yes;
+
+}
+
+
+int main() {
+    fast();
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+
+    // Alhamdulillah
+}
