93e66dbef49495a7d8bf13dd5bb573d07c646763
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-06-12 22:08:36 +0600

    added new contest on codeforces round 1030

---
+++ Codeforces/Round 1030 (Div. 2)/B_Make_It_Permutation.cpp
@@ -0,0 +1,78 @@
+/*
+||-----------------------------------||
+||        Abu Bakar Istiak           ||
+||     Chattogram Polytechnic        ||
+||        Department of CST          ||
+||    abubakar119147@gmail.com       ||
+||-----------------------------------||
+*/
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define ull unsigned long long
+#define pi pair<ll, ll>
+#define vi vector<ll>
+#define vpi vector<pi>
+#define pb push_back
+#define endl "\n"
+#define sp " "
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define sz(x) (ll)(x).size()
+#define minus cout << -1 << endl
+#define zero cout << 0 << endl
+#define MAX 100000
+#define MOD 998244353
+#define afor(i,a,b) for (ll i = (a); i < (b); ++i)
+#define rfor(i,a,b) for (ll i = (a); i >= (b); --i)
+#define asort(v) sort((v).begin(), (v).end())
+#define rsort(v) sort((v).begin(), (v).end(), greater<>())
+#define fast() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+struct op{
+    int i,j,k;
+};
+
+
+void solve() {
+    int n; cin >> n;
+    vector<op> v;
+    v.push_back({1,1,n});
+
+    for(int i=2; i<=n; i++){
+        int left=n-i+1;
+        int right=left+1;
+
+        v.push_back({i,1,left});
+        if(left<=n){
+            v.push_back({i,right,n});
+        }
+    }
+    cout << v.size() << endl;
+    for(const auto &it:v){
+        cout << it.i << " " << it.j << " " << it.k << endl;
+    }
+}
+
+
+int main() {
+    fast();
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+
+    // Alhamdulillah
+}
